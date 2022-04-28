1a521a27b61856cdc31ff55a75dfb05119c7f1da
Joshua Christianson <macgyverjunk@yahoo.com>
2022-04-28 17:54:35 -0500

    application fully functional.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const notesRouter = require('./notes');
+
+const diagnosticsRouter = require('./diagnostics');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+app.use('/diagnostics', diagnosticsRouter);
+
+module.exports = app;
